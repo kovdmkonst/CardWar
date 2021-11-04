32357dfbe2bbaf4d4b84a907d7ec110e8d774f4d
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-11-04 16:46:28 +1000

    Initial Commit

---
+++ CardWar/CardWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWarApp.swift
+//  CardWar
+//
+//  Created by Kovalyov Dmitry on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
